31cf2aa43bf2ce72262d3c3fc5886230b7e927d4
Kamz <kamil.insan@outlook.com>
2024-09-25 09:03:53 +0200

    theme

--- themes/sidebar/views/layouts/sidebar.php
+++ themes/sidebar/views/layouts/sidebar.php
@@ -9,52 +9,39 @@
 
 <!-- Sidebar  -->
 <nav id="sidebar">
-	<div class="sidebar-header row mx-0">
-          <div class="col-md-3 pr-0 text-right">
-               <img src="app/static/blank-user.png" alt="" class="img-fluid user-icon">
-          </div>
-          <div class="col-md-9">
-               <h5 style="font-size:15px" class="mt-1 mb-1 text-capitalize"><b><?php echo Yii::app()->user->isGuest ? 'Username' : Yii::app()->user->info['username'];?></b></h5>
-               <h6 style="font-size:14px" class="mt-1"><?php echo  Yii::app()->user->isGuest ? 'email@test.com' : Yii::app()->user->info['email'];?></h6>
-               <span class="badge badge-secondary badge-jabatan">
-                    <b><?php echo Yii::app()->user->isGuest ? 'Role' : Yii::app()->user->info['role'];?></b>
-               </span>
-          </div>
-          <div class="col-md-12">
-               <a class="btn btn-logout w-100" href="index.php?r=sys/changePass" title="Change Password"><i class="fa fa-key"></i> Change Password</a>
-          </div>
+	<div class="sidebar-header">
+				<h4><b><?php echo Yii::app()->user->info['username'];?></b></h4>
+				<h6><?php echo @Yii::app()->user->info['email'];?></h6>
+				<h6><b><?php echo "As ".Yii::app()->user->info['role'];?></b></h6>
+	
 	</div>
 	<ul class="list-unstyled components">
 		<?php
             $html = '';
-          
-            if(is_array(Yii::app()->user->info) && (sizeof(Yii::app()->user->info) > 1)){ //if menu items available open bracket
+            if((sizeof(Yii::app()->user->info) > 1)){ //if menu items available open bracket
                 foreach($menu as $k => $v){
                    if($k > 1){
                         if($v['label'] == '---'){
                         } else {
 							 $html .= '<li>';
                              if(isset($v['url'])){
-                                 $active = '';
                                  $url = is_array($v['url']) ? $v['url'][0] : $v['url'];
-                                 if( $_GET['r'] == $url){
-                                   $active = 'active';
-                                 }
                                  $url = Yii::app()->createUrl($url);
+								 
 								 if(isset($v['items'])){
-									$html .= '<a href="#' . $k.'_'.str_replace(" ", "_",$v['label']) .'"  data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" ><span class="icon-side"><i class="fa '.$v['icon'].'"></i> '.'</span>'.$v['label'];	 
+									$html .= '<a href="#' . $k.'_'.str_replace(" ", "_",$v['label']) .'"  data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" ><i class="fa '.$v['icon'].'"  style="margin-left:10px; margin-right:10px"></i> '.$v['label'];	 
 								 } else {
-                                    $html .= '<a class="'.$active.'" href="' . $url .'"><span class="icon-side"><i class="fa '.$v['icon'].'" ></i> </span>'.$v['label'];	
+                                    $html .= '<a href="' . $url .'"><i class="fa '.$v['icon'].'" style="margin-left:10px; margin-right:10px"></i> '.$v['label'];	
 								 }          
                              } else {
 								 if(isset($v['items'])){
-									$html .= '<a href="#' . $k.'_'.str_replace(" ", "_",$v['label']) .'"  data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" ><span class="icon-side"><i class="fa '.$v['icon'].'"></i> '.'</span>'.$v['label'];
+									$html .= '<a href="#' . $k.'_'.str_replace(" ", "_",$v['label']) .'"  data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" ><i class="fa '.$v['icon'].'"  style="margin-left:10px; margin-right:10px"></i> '.$v['label'];	 
 								 } else {
-                                    $html .= '<a href="#"><span class="icon-side"><i class="fa '.$v['icon'].'"></i> </span>'.$v['label'];	
+                                    $html .= '<a href="#"><i class="fa '.$v['icon'].'" style="margin-left:10px; margin-right:10px"></i> '.$v['label'];	
 								 }
                              }
                              if(isset($v['items'])){
-                                  $html .=  ' <i class="fa fa-chevron-down sub-icon"></i>';
+                                  $html .=  ' <i class="fa fa-chevron-right" style="float:right;"></i>';
                              }
                              $html .= '</a>';
                              if(isset($v['items'])){
@@ -70,9 +57,6 @@
          ?>
 		
 	</ul>
-     <div class="text-center v-logout">
-          <a class="btn btn-outline-danger w-100" href="index.php?r=site/logout" title="Logout"><i class="fa fa-sign-out"></i> Logout</a>
-     </div>
 </nav>
 
 <?php }?>
--- themes/sidebar/views/layouts/topcontent.php
+++ themes/sidebar/views/layouts/topcontent.php
@@ -1,9 +1,9 @@
-<ul class="ds-me ds-nav">
+<ul class="ds-me ds-nav" style="height:60px; margin-bottom:0px;margin-top: 5px;">
     <?php
-        if(is_array(Yii::app()->user->info) && (sizeof(Yii::app()->user->info) > 1)){ //if menu items available open bracket
+        if((sizeof(Yii::app()->user->info) > 1)){ //if menu items available open bracket
 
     ?>
-    <!-- <li class="flyout" style="padding: 10px; vertical-align: super;">
+    <li class="flyout" style="padding: 10px; vertical-align: super;">
         <?php 
             echo '<a href="#">'.strtoupper(Yii::app()->user->info['username']).' <i class="fa fa-chevron-down"></i></a>';                                 
         ?>  
@@ -37,13 +37,17 @@
 				}
 				?>                
         </ul>
-	</li> -->
-     <li>
-        <h5 class="color-text mt-0 mb-1">Dashboard</h5>
-        <div class="color-text time-bar">
-            <span id="jam"></span> - 
-            <span id="tanggal"></span>
-        </div>
+	</li>
+	<li style="margin-top: 0px; padding: 10px; vertical-align: super;">
+        <a href="index.php?r=sys/changePass" title="Change Password"><i class="fa fa-key"></i></a>
+    </li>        
+    <li style="margin-top: 0px; padding: 10px; vertical-align: super;">
+        <a href="index.php?r=site/logout" title="Logout"><i class="fa fa-sign-out"></i></a>
+    </li>         
+     <li style="padding-left: 10px; border-left: 1px solid #bbb; text-align:right; line-height:unset; margin-top:10px;">
+         <span id="tanggal"></span>
+         <br/>
+          <span id="jam"></span>
      </li>     
 	 
 	
